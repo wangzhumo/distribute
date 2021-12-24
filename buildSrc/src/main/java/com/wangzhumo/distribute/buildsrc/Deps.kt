2a650837375a9b5627b10204f97ca5c80a777e8a
wangzhumo <wangzhumoo@gmail.com>
2021-12-24 21:34:24 +0800

    [feature]version config

---
+++ buildSrc/src/main/java/com/wangzhumo/distribute/buildsrc/Deps.kt
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wangzhumo.distribute.buildsrc
+
+object Versions {
+    const val ktlint = "0.43.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.4"
+    const val jdkDesugar = "com.android.tools:desugar_jdk_libs:1.1.5"
+
+    const val junit = "junit:junit:4.13"
+
+    const val material = "com.google.android.material:material:1.3.0"
+
+    object Accompanist {
+        const val version = "0.20.3"
+        const val permissions = "com.google.accompanist:accompanist-permissions:$version"
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+        const val insets_ui = "com.google.accompanist:accompanist-insets-ui:$version"
+        const val viewpager = "com.google.accompanist:accompanist-pager:$version"
+        const val indicators = "com.google.accompanist:accompanist-pager-indicators:$version"
+        const val swiperefresh = "com.google.accompanist:accompanist-swiperefresh:$version"
+        const val systemuicontroller = "com.google.accompanist:accompanist-systemuicontroller:$version"
+    }
+
+    object Kotlin {
+        const val version = "1.5.21"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.5.2"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.3.0"
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+
+        object Lifecycle {
+            private const val version = "2.4.0"
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        }
+
+        object Compose {
+            const val snapshot = ""
+            const val version = "1.0.1"
+
+            @get:JvmStatic
+            val snapshotUrl: String
+                get() = "https://androidx.dev/snapshots/builds/$snapshot/artifacts/repository/"
+
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val test = "androidx.compose.test:test-core:$version"
+            const val uiTest = "androidx.compose.ui:ui-test:$version"
+            const val paging = "androidx.paging:paging-compose:$version"
+        }
+
+        object Paging {
+            private const val paging_version = "3.1.0"
+            const val paging_runtime = "androidx.paging:paging-runtime:$paging_version"
+            const val paging_compose = "androidx.paging:paging-compose:1.0.0-alpha14"
+        }
+
+        object Navigation {
+            private const val version = "2.3.5"
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
+            const val uiKtx = "androidx.navigation:navigation-ui-ktx:$version"
+            const val compose = "androidx.navigation:navigation-compose:2.4.0-rc01"
+        }
+
+        object Material {
+            private const val version = "1.3.0"
+            const val material = "com.google.android.material:material:$version"
+        }
+
+        object Test {
+            private const val version = "1.4.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+
+        object Splash{
+            private const val version = "1.4.0"
+            const val splashScreen = "androidx.core:core-splashscreen:1.0.0-alpha01"
+        }
+    }
+
+    object Retrofit{
+        private const val version = "2.9.0"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val retrofit_gson = "com.squareup.retrofit2:converter-gson:$version"
+        const val retrofit_scalars = "com.squareup.retrofit2:converter-scalars:$version"
+    }
+
+    object Okhttp{
+        private const val version = "4.9.0"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val okhttp_logger = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object Coil {
+        const val coilCompose = "io.coil-kt:coil-compose:1.4.0"
+    }
+}
